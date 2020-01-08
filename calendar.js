e525be39369149729120fc8de6d750de0910915c
seungwooKim99 <sonic886@naver.com>
2020-01-08 15:28:36 +0900

    2nd commit -Clickable days -save two selectedDays in var -modified selectUser() using addEventListener method

--- calendar.js
+++ calendar.js
@@ -1,9 +1,17 @@
 var monthNames = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var weekNames = ['Sun', 'Mon', 'Tue', 'Wed','Thu', 'Fri', 'Sat'];
+var jsDays;
+var selectedDay1 = 0;
+var selectedDay2 = 0;
 
 const weekCalendar = document.querySelector(".weekCalendar");
-const userSelect = document.querySelector(".userSelect");
+const userList = document.querySelector(".userList");
 const newTr2 = document.createElement("TR");
+
+const user1 = userList.querySelector(".user1");
+const user2 = userList.querySelector(".user2");
+const user3 = userList.querySelector(".user3");
+
 
 function calendar(){
     /* That's how creatElement() works!
@@ -53,6 +61,7 @@ function calendar(){
    for(var i = 1; i <= 7; i++){
        const newCell = document.createElement("DIV");
        newCell.id = i;
+       newCell.className = `jsDay`;
        newCell.innerText = `${weekNames[i-1]}`;
        newCell.style.border = "1px solid";
        newCell.style.borderColor = "#874f1e";
@@ -63,23 +72,47 @@ function calendar(){
        weekCalendar.appendChild(newCell);
    }
    weekCalendar.style.display = "flex";
+   jsDays = document.getElementsByClassName("jsDay");
 }
 
-function selectDate(evemt){
-    console.log(event);
+function makeDaysToArray(userSelected){
+    console.log(userSelected.target.value);
+    /*
+    Array.from(jsDays).forEach(date =>
+        date.addEventListener("click", selectDate)
+   );
+   */
+   const daysArray = Array.from(jsDays);
+   daysArray.forEach(date =>
+    date.addEventListener("click", selectDate)
+    ); 
 }
 
-function calculateResult(){
-    //console.log(userSelect);
-    const user1 = userSelect.querySelector(".user1");
-    const user2 = userSelect.querySelector(".user2");
-    const user3 = userSelect.querySelector(".user3");
+function selectDate(event){
+    //console.log(event.target.id);
+    if(selectedDay1 === 0){
+        selectedDay1 = event.target.id;
+    }
+    else{
+        selectedDay2 = event.target.id;
+    }
+    //console.log(selectedDay1);
+    //console.log(selectedDay2);
+    
+}
 
-    //console.log(user1.checked);
-    console.log(Array.from(weekCalendar));
+function selectUser(){
+    //console.log(userList);
+    
+    user1.addEventListener("click", makeDaysToArray);
+    user2.addEventListener("click", makeDaysToArray);
+    user3.addEventListener("click", makeDaysToArray);
+
+    /*
+    user1.addEventListener("click", selectDate)
     if(user1.checked){
         //console.log(weekCalendar);
-        Array.from(weekCalendar).forEach(date =>
+        Array.from(jsDays).forEach(date =>
              date.addEventListener("click", selectDate)
         );
         //addEventListener("clock", selectDate);
@@ -90,11 +123,12 @@ function calculateResult(){
     else{
         console.log("user3 checked");
     }
+    */
 }
 
 function init(){
     calendar();
-    calculateResult();
+    selectUser();
 }
 
 init();

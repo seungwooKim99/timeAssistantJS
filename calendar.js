397205740d1fb63a232f1cae3a2d903494dd50f7
seungwooKim99 <sonic886@naver.com>
2020-01-09 12:58:02 +0900

    3rd commit -prototype complete for 3 users -show result on console

--- calendar.js
+++ calendar.js
@@ -1,8 +1,12 @@
 var monthNames = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var weekNames = ['Sun', 'Mon', 'Tue', 'Wed','Thu', 'Fri', 'Sat'];
 var jsDays;
-var selectedDay1 = 0;
-var selectedDay2 = 0;
+var user1SelectFlag1 = false;
+var user2SelectFlag1 = false;
+var user3SelectFlag1 = false;
+var resultFlag = false;
+
+var selectedDaysList = [0, 0, 0, 0, 0, 0];
 
 const weekCalendar = document.querySelector(".weekCalendar");
 const userList = document.querySelector(".userList");
@@ -82,23 +86,168 @@ function makeDaysToArray(userSelected){
         date.addEventListener("click", selectDate)
    );
    */
+  switch(userSelected.target.value){
+      case 'user1':
+          user1SelectFlag1 = true;
+          break;
+      case 'user2':
+          user2SelectFlag1 = true;
+          break;
+      case 'user3':
+          user3SelectFlag1 = true;
+          break;
+  }
    const daysArray = Array.from(jsDays);
+   //console.log(daysArray);
    daysArray.forEach(date =>
     date.addEventListener("click", selectDate)
-    ); 
+    );
+}
+
+function checkIfAllSelected(){
+    console.log(selectedDaysList);
+    for(var i = 0;i<6;i++){
+        if(selectedDaysList[i] === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+function swap(a, b){
+
+}
+
+function sortLowercase(){
+    var temp = 0;
+    if(selectedDaysList[0]>selectedDaysList[2]){
+        temp = selectedDaysList[0];
+        selectedDaysList[0] = selectedDaysList[2];
+        selectedDaysList[2] = temp;
+
+        temp = selectedDaysList[1];
+        selectedDaysList[1] = selectedDaysList[3];
+        selectedDaysList[3] = temp;
+
+        if(selectedDaysList[0]>selectedDaysList[4]){
+            temp = selectedDaysList[0];
+            selectedDaysList[0] = selectedDaysList[4];
+            selectedDaysList[4] = temp;
+
+            temp = selectedDaysList[1];
+            selectedDaysList[1] = selectedDaysList[5];
+            selectedDaysList[5] = temp;
+        }
+        if(selectedDaysList[2]>selectedDaysList[4]){
+            temp = selectedDaysList[2];
+            selectedDaysList[2] = selectedDaysList[4];
+            selectedDaysList[4] = temp;
+
+            temp = selectedDaysList[3];
+            selectedDaysList[3] = selectedDaysList[5];
+            selectedDaysList[5] = temp;
+        }
+    }
+    else{
+        if(selectedDaysList[0]>selectedDaysList[4]){
+            temp = selectedDaysList[0];
+            selectedDaysList[0] = selectedDaysList[4];
+            selectedDaysList[4] = temp;
+
+            temp = selectedDaysList[1];
+            selectedDaysList[1] = selectedDaysList[5];
+            selectedDaysList[5] = temp;
+        }
+        if(selectedDaysList[2]>selectedDaysList[4]){
+            temp = selectedDaysList[2];
+            selectedDaysList[2] = selectedDaysList[4];
+            selectedDaysList[4] = temp;
+
+            temp = selectedDaysList[3];
+            selectedDaysList[3] = selectedDaysList[5];
+            selectedDaysList[5] = temp;
+        }
+    }
+}
+
+function calculateResult(){
+    sortLowercase();
+    var start = 0;
+    var finish = 0;
+    console.log(selectedDaysList);
+    if(selectedDaysList[1]<selectedDaysList[3]){
+        start = selectedDaysList[2];
+        finish = selectedDaysList[1];
+    }
+    else{
+        start = selectedDaysList[2];
+        finish = selectedDaysList[3];
+    }
+
+    if(finish<selectedDaysList[5]){
+        start = selectedDaysList[4];
+    }
+    else{
+        start = selectedDaysList[4];
+        finish = selectedDaysList[5];
+    }
+
+    if(start<=finish){
+        for(var i = start-1; i<finish; i++){
+            console.log(weekNames[i]);
+        }
+    }
+    else{
+        console.log(`We cannot meet!`);
+    }
 }
 
 function selectDate(event){
     //console.log(event.target.id);
-    if(selectedDay1 === 0){
-        selectedDay1 = event.target.id;
+
+    /*
+    if(user1SelectFlag1 === 0){
+        user1SelectedDay1 = event.target.id;
     }
     else{
-        selectedDay2 = event.target.id;
+        user1SelectedDay2 = event.target.id;
     }
-    //console.log(selectedDay1);
+    */
+    if(user1SelectFlag1 === true){
+        if(selectedDaysList[0] != 0){
+            selectedDaysList[1] = event.target.id;
+            user1SelectFlag1 = false;
+        }
+        else{
+            selectedDaysList[0] = event.target.id;
+        }
+    }
+    if(user2SelectFlag1 === true){
+        if(selectedDaysList[2] != 0){
+            selectedDaysList[3] = event.target.id;
+            user2SelectFlag1 = false;
+        }
+        else{
+            selectedDaysList[2] = event.target.id;
+        }
+    }
+    if(user3SelectFlag1 === true){
+        if(selectedDaysList[4] != 0){
+            selectedDaysList[5] = event.target.id;
+            user3SelectFlag1 = false;
+        }
+        else{
+            selectedDaysList[4] = event.target.id;
+        }
+    }
+    //console.log(user1SelectedDay1);
     //console.log(selectedDay2);
     
+    resultFlag = checkIfAllSelected();
+    console.log(resultFlag);
+    if(resultFlag === true){
+        calculateResult();
+    }
 }
 
 function selectUser(){
@@ -108,6 +257,7 @@ function selectUser(){
     user2.addEventListener("click", makeDaysToArray);
     user3.addEventListener("click", makeDaysToArray);
 
+    
     /*
     user1.addEventListener("click", selectDate)
     if(user1.checked){
